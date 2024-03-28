ca81a5f36e6b7a89e10501ca95b955d8ecae39d9
re-nanashi <jfabro3124@gmail.com>
2024-03-29 04:45:50 +0800

    chore: Remove comments

--- src/textnode.py
+++ src/textnode.py
@@ -25,16 +25,6 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-"""
-text_type_text: This should become a LeafNode with no tag, just a raw text value.
-text_type_bold: This should become a LeafNode with a "b" tag and the text
-text_type_italic: "i" tag, text
-text_type_code: "code" tag, text
-text_type_link: "a" tag, anchor text, and "href" prop
-text_type_image: "img" tag, empty string value, "src" and "alt" props ("src" is the image URL, "alt" is the alt text)
-"""
-
-
 def text_node_to_html_node(text_node):
     match text_node.text:
         case TextType.text_type_text:
@@ -51,32 +41,3 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception(f"Invalid text type: {text_node.text_type}")
-
-
-"""
-node = TextNode("This is text with a `code block` word", text_type_text)
-new_nodes = split_nodes_delimiter([node], "`", text_type_code)
-
-It returns:
-[
-    TextNode("This is text with a ", text_type_text),
-    TextNode("code block", text_type_code),
-    TextNode(" word", text_type_text),
-"""
-
-
-def split_nodes_delimiter(old_nodes, delimiter, text_type_code):
-    new_nodes = []
-    for node in old_nodes:
-        if not isinstance(node, TextNode):
-            new_nodes.append(new_nodes)
-        res_node = []
-        split_text_str = node.text.split(delimiter)
-        if len(split_text_str) != 3:
-            raise Exception(
-                "Invalid markdown syntax: matching closing delimiter not found")
-        res_node.append(TextNode(split_text_str[0], TextType.text_type_text))
-        res_node.append(TextNode(split_text_str[1], text_type_code))
-        res_node.append(TextNode(split_text_str[2], TextType.text_type_text))
-        new_nodes.extend(res_node)
-    return new_nodes
