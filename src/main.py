f216296f7d1fdcd441149981b2b2fcbd1b54f389
re-nanashi <jfabro3124@gmail.com>
2024-03-31 04:25:17 +0800

    feat: Copy static files to public then generate site

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copystatic import copy_files_r
+from gencontent import generate_page
+
+
+def copy_static_files():
+    dir_path_static = "./static"
+    dir_path_public = "./public"
+    if not os.path.exists(dir_path_static):
+        # Raise an exception if no folder to copy
+        raise Exception("Directory does not exist: static. Exiting program...")
+    if not os.path.exists(dir_path_public):
+        # Create new dir if public directory does not exist
+        os.mkdir("../public")
+    # Delete the public dir's content
+    shutil.rmtree(dir_path_public)
+    # Copy the files from static to public
+    copy_files_r(dir_path_static, dir_path_public)
+    print("Success: All files from the static directory has been copied to the public directory.")
 
 
 def main():
-    tnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(tnode)
+    copy_static_files()
+    generate_page("./content/index.md", "./template.html",
+                  "./public/index.html")
 
 
 main()
